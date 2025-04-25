4583e97c89bf58644571eed0c1d0293b7e077a93
logles <lydialogles@gmail.com>
2025-04-24 18:38:32 -0600

    upload base code and file structure

---
+++ client/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});

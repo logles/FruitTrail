0d9bb821616cdda4bd3f0c7329fd94c696cb0ef7
bret jensen <jensenbret13@gmail.com>
2025-04-28 20:18:38 -0700

    header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // uncommment once Nav component is done --- if needed ???
 // import Navigation from './Navigation.jsx';
+import TreeLogo from './TreeLogo';
 
 function Header() {
   return (
     <header>
+        <TreeLogo />
         <h1>FruitTrail</h1>
         {/* <Navigation /> */}
         {/* importing navigation component into header */}

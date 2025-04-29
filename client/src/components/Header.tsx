e1d5559f4554c50a899d4833fb544ba29c566d5d
bret jensen <jensenbret13@gmail.com>
2025-04-28 20:14:25 -0700

    components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// uncommment once Nav component is done --- if needed ???
+// import Navigation from './Navigation.jsx';
+
+function Header() {
+  return (
+    <header>
+        <h1>FruitTrail</h1>
+        {/* <Navigation /> */}
+        {/* importing navigation component into header */}
+    </header>
+);
+}
+
+
+export default Header;
---
+++ client/src/components/TreeLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TreeLogo: React.FC = () => {
+    return (
+        <img src="./assets/logo.png" alt="logo for FruitTrail" />
+    );
+};
+
+export default TreeLogo;

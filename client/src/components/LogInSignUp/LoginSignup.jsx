8029f5ae0c3b7a6ecd98bc1e698c660b0b57d118
bret jensen <jensenbret13@gmail.com>
2025-04-29 19:49:39 -0700

    loginsignup

--- client/src/components/LogInSignUp/LoginSignup.jsx
+++ client/src/components/LogInSignUp/LoginSignup.jsx
@@ -11,14 +11,19 @@ const LoginSignup = () => {
             <div className="inputs">
                 <div className="input">
                     {/* <img src={to use if we want icons} alt=""/> */}
-                    <input type="text" />
+                    <input type="text" placeholder='name' />
                 </div>
                 <div className="input">
-                    <input type="email" />
+                    <input type="email" placeholder='email'/>
                 </div>
                 <div className="input">
-                    <input type="password" />
+                    <input type="password" placeholder='password' />
                 </div>
+            </div>
+            <div className="forgot-password">forgot password? <span>click here</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
             </div>
         </div>
     )

41d37250a275ecbc88f4b338cacd46a992e0fde1
bret jensen <jensenbret13@gmail.com>
2025-05-04 14:54:22 -0700

    quieries and mutations, more styling

--- client/src/components/ControlPanel/ControlPanel.tsx
+++ client/src/components/ControlPanel/ControlPanel.tsx
@@ -4,43 +4,65 @@ import { useState } from "react";
 
 function ControlPanel() {
     const [showControlMobile, setShowControlMobile] = useState(false)
+    const [showBurgerButton, setshowBurgerButton] = useState(true)
 
     function showControlPanelMobile() {
         setShowControlMobile(!showControlMobile)
+        setshowBurgerButton(showControlMobile)
     }
 
     return (
         <>
-            <div id="control-panel">
-                <button id="A" className="control-button">
-                    <i className="fa-solid fa-plus"></i>
-                </button>
-                <button id="B" className="control-button">
-                    <i className="fa-solid fa-user"></i>
-                </button>
-                <button id="C" className="control-button">
-                    <i className="fa-solid fa-magnifying-glass-location"></i>
-                </button>
-                <button id="D" className="control-button">
-                    <i className="fa-solid fa-star"></i>
-                </button>
-                <button id="burger-button" className="control-button" onClick={showControlPanelMobile}>
-                    <i className="fa-solid fa-bars"></i>
-                </button>
-            </div>
+            {
+                showBurgerButton ? (
+                    <div id="control-panel">
+                    <button id="A" className="control-button">
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
+                    <button id="B" className="control-button">
+                        <i className="fa-solid fa-user"></i>
+                    </button>
+                    <button id="C" className="control-button">
+                        <i className="fa-solid fa-magnifying-glass-location"></i>
+                    </button>
+                    <button id="D" className="control-button">
+                        <i className="fa-solid fa-star"></i>
+                    </button>
+                    <button id="burger-button" className="control-button" onClick={showControlPanelMobile}>
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+                </div>
+                ) : null
+            }
             {
                 showControlMobile ? (
                     <div id="control-panel-mobile">
-                        <button className="control-button">
+                        <button onClick = {
+                            ()=>{
+                                showControlPanelMobile()
+                            }
+                        } className="control-button">
                             <i className="fa-solid fa-plus"></i>
                         </button>
-                        <button className="control-button">
+                        <button onClick = {
+                            ()=>{
+                                showControlPanelMobile()
+                            }
+                        } className="control-button">
                             <i className="fa-solid fa-user"></i>
                         </button>
-                        <button className="control-button">
+                        <button onClick = {
+                            ()=>{
+                                showControlPanelMobile()
+                            }
+                        } className="control-button">
                             <i className="fa-solid fa-magnifying-glass-location"></i>
                         </button>
-                        <button className="control-button">
+                        <button onClick = {
+                            ()=>{
+                                showControlPanelMobile()
+                            }
+                        } className="control-button">
                             <i className="fa-solid fa-star"></i>
                         </button>
                     </div>
--- client/src/components/LogInSignUp/LoginSignup.jsx
+++ client/src/components/LogInSignUp/LoginSignup.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
 import './LoginSignup.css'
+import { ADD_USER } from '@/utils/mutations';
+import { LOGIN_USER } from '@/utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const LoginSignup = () => {
-
+    const [addUser] = useMutation(ADD_USER);
+    const [logInUser] = useMutation(LOGIN_USER);
     const [action, setAction] = useState("Sign Up");
+    const [username, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    function handleSubmit() {
+        if (action == "Sign Up"){
+            addUser({
+                variables: {  
+                    "username": username,
+                    "email": email,
+                    "password": password
+                  }
+        
+            })
+        }
+        else {
 
+        }
+    }
     return (
         
         <div className="container">
@@ -16,16 +37,25 @@ const LoginSignup = () => {
             <div className="inputs">
                 {/* action for hiding the name field on the login portion */}
                 {action === "Login" ? <div></div> : <div className="input">
-                    <input type="text" placeholder='name' />
+                    <input type="text" placeholder='username' value = {username} onChange = {(event)=>{
+                        setUserName(event.target.value)
+                    }}/>
                 </div>}
                 <div className="input">
-                    <input type="email" placeholder='email' />
+                    <input type="email" placeholder='email' value = {email} onChange = {(event)=>{
+                        setEmail(event.target.value)
+                    }}/>
                 </div>
                 <div className="input">
-                    <input type="password" placeholder='password' />
+                    <input type="password" placeholder='password' value = {password} onChange = {(event)=>{
+                        setPassword(event.target.value)
+                    }}/>
                 </div>
             </div>
             {action === "Sign Up" ? <div></div> : <div className="forgot-password">forgot password? <span>click here</span></div>}
+            <div className = "submit-container">
+                <button onClick = {handleSubmit}className ="submit">Submit</button>
+            </div>
             <div className="submit-container">
                 {/* action that highlights the button for the page you are on */}
                 <div className={action === "Login" ? "submit grey" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
